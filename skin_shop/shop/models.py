8faed9f57d04b4673630b13c1d4f4630b5430d0a
markgardie <60879509+markgardie@users.noreply.github.com>
2025-11-22 19:59:30 +0200

    add created_at and update_at fields to models

--- skin_shop/shop/models.py
+++ skin_shop/shop/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.utils.text import slugify
 
-
 class SkinCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=120, unique=True, blank=True)
-
     description = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)     
 
     class Meta:
         verbose_name = "Skin Category"
@@ -26,6 +26,8 @@ class SkinCategory(models.Model):
 class SkinTag(models.Model):
     name = models.CharField(max_length=30, unique=True)
     slug = models.SlugField(max_length=40, unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)     
 
     class Meta:
         verbose_name = "Skin Tag"
@@ -50,32 +52,27 @@ class Skin(models.Model):
 
     name = models.CharField(max_length=120)
     slug = models.SlugField(max_length=150, unique=True, blank=True)
-
     category = models.ForeignKey(
         SkinCategory,
         on_delete=models.SET_NULL,
         null=True,
         related_name="skins"
     )
-
     tags = models.ManyToManyField(SkinTag, blank=True)
-
     type = models.CharField(
         max_length=20,
         choices=SkinType.choices,
         default=SkinType.REGULAR
     )
-
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
-
     image = models.ImageField(upload_to="skins/previews/")
     file = models.FileField(
         upload_to="skins/files/",
         help_text="Скін у форматі PNG"
     )
-
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)     
 
     class Meta:
         verbose_name = "Skin"
