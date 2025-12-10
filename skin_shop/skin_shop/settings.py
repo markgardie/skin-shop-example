cf2dbb45567b01bf1ce34bce17530ae6d25cc17d
markgardie <60879509+markgardie@users.noreply.github.com>
2025-12-10 17:07:26 +0200

    config db

--- skin_shop/skin_shop/settings.py
+++ skin_shop/skin_shop/settings.py
@@ -28,7 +28,22 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
 
-
+# DATABASES
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    # Використовуємо PostgreSQL на Render
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+    }
+else:
+    # Локально SQLite
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 # Application definition
 
 INSTALLED_APPS = [
